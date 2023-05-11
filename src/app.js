e3ca5b534d16608d42e1118b05ef5dc44e9ef892
Igor M. Santos <igormsprofissional@gmail.com>
2023-05-11 17:57:17 -0300

    Processo Cadastro Médicos Completo

--- src/app.js
+++ src/app.js
@@ -1,35 +1,98 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs');
+const path = require('path');
+
 const Sequelize = require('sequelize')
 const { sequelize } = require('./backend/models/db');
 const Medicos = require('./backend/models/Medicos');
 
+const bodyParser = require('body-parser')
+
+const medico = require('../src/backend/models/Medicos')
+
+//Configuração BodyParser
+app.use(bodyParser.urlencoded({ extended: false }))
+//Parse application/json
+app.use(bodyParser.json())
 
 
-app.get("/cadastroMedicos", function(req,res){
-  res.sendFile(__dirname +"/frontend/views/cadastroMedicos.html");
+//Rotas
+app.get('/cadastroMedicos', function (req, res) {
+  const filePath = path.join(__dirname, '../src/frontend/views/cadastroMedicos.html');
+  fs.readFile(filePath, function (err, content) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Erro ao carregar o arquivo.');
+    } else {
+      res.send(content.toString());
+    }
+  });
+});
 
-})
 
-sequelize.authenticate().then(function() {
+//Receber dados do formulário
+app.post('/medicoCadastrado', function (req, res) {
+  const filePath = path.join(__dirname, '../src/frontend/views/medicoCadastrado.html');
+  fs.readFile(filePath, function (err, content) {
+
+    medico.create({
+      nome_medico: req.body.nome,
+      uf_medico: req.body.uf,
+      crm_medico: req.body.crm,
+      especialidade_medico: req.body.especialidade,
+      situacao_medico: req.body.situacao,
+      telefone_medico: req.body.telefone,
+      email_medico: req.body.email,
+    }).then(function () {
+      res.send("Médico cadastrado: "+ req.body.nome)
+    }).catch(function (erro) {
+      res.send("Erro ao cadastrar médico!" + erro)
+    })
+  });
+});
+
+
+
+sequelize.authenticate().then(function () {
   console.log("Conexeão realizada com sucesso")
-}).catch(function(err){
-  console.log("Erro ao realizar a conexão com banco de dados: " +err)
+}).catch(function (err) {
+  console.log("Erro ao realizar a conexão com banco de dados: " + err)
 })
 
 
 //Add médico na tabela
-// Medicos.create({
-//   email_medico: "julia@gmail.com",
-//   especialidade_medico: "Louco",
-//   nome_medico: "Julia",
-//   telefone_medico: "9999999",
-//   uf_medico: "BA",
-//   crm_medico: "846743",
-//   situacao_medico: "Ativo"
-// })
+//  Medicos.create({
+//    email_medico: "julia@gmail.com",
+//    especialidade_medico: "Louco",
+//    nome_medico: "Julia",
+//    telefone_medico: "9999999",
+//    uf_medico: "BA",
+//    crm_medico: "846743",
+//    situacao_medico: "Ativo"
+//  })
 
+
+// Função para excluir um usuário pelo ID
+// async function deleteUserById(id_medicos) {
+//   try {
+//     const rowsDeleted = await Medicos.destroy({
+//       where: { id_medicos }, // Identifica o registro pelo ID
+//     });
+
+//     if (rowsDeleted === 0) {
+//       console.log(`Nenhum usuário encontrado com o ID ${id_medicos}`);
+//     } else {
+//       console.log(`Usuário com o ID ${id_medicos} excluído com sucesso`);
+//     }
+//   } catch (error) {
+//     console.error('Erro ao excluir usuário:', error);
+//   }
+// }
+
+// // Chama a função para excluir um usuário com ID 1
+// deleteUserById(7);
 
 
 app.listen(8080, () => {
@@ -41,11 +104,11 @@ app.listen(8080, () => {
 
 
 //Inserindo novo médico
-// connection.query("INSERT INTO medicos(email_medico, especialidade_medico, nome_medico, telefone_medico, uf_medico, crm_medico, situacao_medico) VALUES ('iris@gmail.com', 'Psicologa', 'Iris', '99999999', 'MG', '432653', 'Ativo')", function(err, result){
-//   if(!err){
+//  connection.query("INSERT INTO medicos(email_medico, especialidade_medico, nome_medico, telefone_medico, uf_medico, crm_medico, situacao_medico) VALUES ('iris@gmail.com', 'Psicologa', 'Iris', '99999999', 'MG', '432653', 'Ativo')", function(err, result){
+//    if(!err){
 //     console.log("Medico cadastrado com sucesso!")
-//   }else{console.log('Erro ao cadastrar medico')}
-// })
+//    }else{console.log('Erro ao cadastrar medico')}
+//  })
 
 
 //Editando médico existente
--- src/frontend/scripts/cadastroMedicos.js
+++ src/frontend/scripts/cadastroMedicos.js
@@ -1,100 +1,100 @@
-// array para armazenar os médicos cadastrados
-let medicos = [];
+// // array para armazenar os médicos cadastrados
+// let medicos = [];
 
 
 
-// função para adicionar um médico na tabela
-function adicionarMedicoNaTabela(medico) {
-  const tableBody = document.querySelector('#medicos-table tbody');
-  const row = tableBody.insertRow();
+// // função para adicionar um médico na tabela
+// function adicionarMedicoNaTabela(medico) {
+//   const tableBody = document.querySelector('#medicos-table tbody');
+//   const row = tableBody.insertRow();
 
-  const nomeCell = row.insertCell(0);
-  const ufCell = row.insertCell(1)
-  const crmCell = row.insertCell(2);
-  const especialidadeCell = row.insertCell(3)
-  const situacaoCell = row.insertCell(4);
-  const telefoneCell = row.insertCell(5);
-  const emailCell = row.insertCell(6);
-  const actionsCell = row.insertCell(7);
+//   const nomeCell = row.insertCell(0);
+//   const ufCell = row.insertCell(1)
+//   const crmCell = row.insertCell(2);
+//   const especialidadeCell = row.insertCell(3)
+//   const situacaoCell = row.insertCell(4);
+//   const telefoneCell = row.insertCell(5);
+//   const emailCell = row.insertCell(6);
+//   const actionsCell = row.insertCell(7);
 
-  nomeCell.innerText = medico.nome;
-  ufCell.innerText = medico.uf;
-  crmCell.innerText = medico.crm;
-  especialidadeCell.innerText = medico.especialidade
-  situacaoCell.innerText = medico.situacao;
-  telefoneCell.innerText = medico.telefone;
-  emailCell.innerText = medico.email;
+//   nomeCell.innerText = medico.nome;
+//   ufCell.innerText = medico.uf;
+//   crmCell.innerText = medico.crm;
+//   especialidadeCell.innerText = medico.especialidade
+//   situacaoCell.innerText = medico.situacao;
+//   telefoneCell.innerText = medico.telefone;
+//   emailCell.innerText = medico.email;
   
-  // botão de excluir médico
-  const deleteButton = document.createElement('button');
-  deleteButton.innerText = 'Excluir';
-  deleteButton.onclick = () => {
-    excluirMedico(medico.crm);
-  }
-  actionsCell.appendChild(deleteButton);
-}
+//   // botão de excluir médico
+//   const deleteButton = document.createElement('button');
+//   deleteButton.innerText = 'Excluir';
+//   deleteButton.onclick = () => {
+//     excluirMedico(medico.crm);
+//   }
+//   actionsCell.appendChild(deleteButton);
+// }
 
-// função para excluir um médico do array e da tabela
-function excluirMedico(crm) {
-  medicos = medicos.filter(medico => medico.crm !== crm);
-  atualizarTabela();
-}
+// // função para excluir um médico do array e da tabela
+// function excluirMedico(crm) {
+//   medicos = medicos.filter(medico => medico.crm !== crm);
+//   atualizarTabela();
+// }
 
-// função para atualizar a tabela com os dados do array de médicos
-function atualizarTabela() {
-  const tableBody = document.querySelector('#medicos-table tbody');
-  tableBody.innerHTML = '';
+// // função para atualizar a tabela com os dados do array de médicos
+// function atualizarTabela() {
+//   const tableBody = document.querySelector('#medicos-table tbody');
+//   tableBody.innerHTML = '';
 
-  medicos.forEach(medico => {
-    adicionarMedicoNaTabela(medico);
-  });
-}
+//   medicos.forEach(medico => {
+//     adicionarMedicoNaTabela(medico);
+//   });
+// }
 
-// função limpar formulário
-function limpar() {
-    document.getElementById("nome").value = "";
-    document.getElementById("uf").value = "";
-    document.getElementById("crm").value = "";
-    document.getElementById("especialidade").value= "";
-    document.getElementById("situacao").value = "";
-    document.getElementById("telefone").value = "";
-    document.getElementById("email").value = "";
-  }
+// // função limpar formulário
+// function limpar() {
+//     document.getElementById("nome").value = "";
+//     document.getElementById("uf").value = "";
+//     document.getElementById("crm").value = "";
+//     document.getElementById("especialidade").value= "";
+//     document.getElementById("situacao").value = "";
+//     document.getElementById("telefone").value = "";
+//     document.getElementById("email").value = "";
+//   }
 
 
-// função pra não recarregar página ao enviar novo médico
-  const form = document.querySelector('form');
+// // função pra não recarregar página ao enviar novo médico
+//   const form = document.querySelector('form');
   
-  form.addEventListener('submit', function(event) {
+//   form.addEventListener('submit', function(event) {
   
-      adicionarMedico()
+//       adicionarMedico()
   
-      event.preventDefault();
+//       event.preventDefault();
   
-  })
+//   })
 
-// função para adicionar um médico ao array e atualizar a tabela
-function adicionarMedico() {
-  const nome = document.querySelector('#nome').value;
-  const uf = document.querySelector('#uf').value
-  const crm = document.querySelector('#crm').value;
-  const especialidade = document.querySelector('#especialidade').value 
-  const situacao = document.querySelector('#situacao').value;
-  const telefone = document.querySelector('#telefone').value;
-  const email = document.querySelector('#email').value;
+// // função para adicionar um médico ao array e atualizar a tabela
+// function adicionarMedico() {
+//   const nome = document.querySelector('#nome').value;
+//   const uf = document.querySelector('#uf').value
+//   const crm = document.querySelector('#crm').value;
+//   const especialidade = document.querySelector('#especialidade').value 
+//   const situacao = document.querySelector('#situacao').value;
+//   const telefone = document.querySelector('#telefone').value;
+//   const email = document.querySelector('#email').value;
 
-  const medico = {
-    nome: nome,
-    uf: uf,
-    crm: crm,
-    especialidade: especialidade,
-    situacao: situacao,
-    telefone: telefone,
-    email: email
-  };
-  console.log(medico)
-  medicos.push(medico);
-  atualizarTabela();
+//   const medico = {
+//     nome: nome,
+//     uf: uf,
+//     crm: crm,
+//     especialidade: especialidade,
+//     situacao: situacao,
+//     telefone: telefone,
+//     email: email
+//   };
+//   console.log(medico)
+//   medicos.push(medico);
+//   atualizarTabela();
 
 
-}
+// }
