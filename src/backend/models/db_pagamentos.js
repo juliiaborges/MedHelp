e42068b1d0eec7b4a1f1a076d07255245247a8ed
Arthur Bretas <arthurcbretas@gmail.com>
2023-05-28 22:01:20 -0300

    CadastroDePagamentos_backend
    Co-authored-by: Gabriel Vitor <GabrielVitor3@users.noreply.github.com>

---
+++ src/backend/models/db_pagamentos.js
@@ -0,0 +1,33 @@
+const db = require('./db.js')
+
+const Pagamentos = db.sequelize.define('pagamentos', {
+    id_pagamentos: {
+        type: db.Sequelize.INTEGER,
+        autoIncrement: true,
+      primaryKey: true
+    },
+    data_pagamento:{
+        type: db.Sequelize.DATEONLY
+    },
+    tipo_pagamento: {
+        type: db.Sequelize.STRING
+    },
+    valor_pagamento: {
+        type: db.Sequelize.DOUBLE
+    },
+    possui_plano: {
+        type: db.Sequelize.STRING
+    },
+    pacientes_cpf_paciente: {
+        type: db.Sequelize.INTEGER,
+        allowNull: true, // permitir valores nulos
+        defaultValue: null // definir o valor padrão como null
+    },
+}, { 
+    timestamps: false
+});
+
+    module.exports = Pagamentos;
+
+//Criar a tabela
+//Pagamentos.sync({force: true})
